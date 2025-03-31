6e3fbc6391ee9e97b2aebcc37968078a6b62f135
ysse <72249363+NicoalsD@users.noreply.github.com>
2025-03-30 21:07:33 -0500

    Create EmpresaTransporte.py

---
+++ EmpresaTransporte.py
@@ -0,0 +1,18 @@
+__author__ = "Nicolas Alejandro Diaz Acosta"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__email__ = "nicolas.diazacost@campusucc.edu.co"
+
+from Camion import Camion
+
+class EmpresaTransporte:
+    #Constantes
+
+    # Relaciones
+
+    camion1 = Camion()
+    camion3 = Camion()
+    camion2 = Camion()
+    camion4 = Camion()
+
+    # Constructor
